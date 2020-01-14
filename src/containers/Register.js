0c38fd89ce025ecbb33af2740da7d56afab35736
Andrés Aguilar Nieto <desagneto@gmail.com>
2020-01-14 12:53:26 -0500

    clase container register

---
+++ src/containers/Register.js
@@ -0,0 +1,41 @@
+import React from 'react';
+
+import '../assets/styles/components/Register.css'
+
+function Register() {
+
+    return(
+        <section className="register">
+            <section className="register__container">
+                <h2>Regístrate</h2>
+                <form className="register__container--form">
+                    <input
+                        name="name"
+                        className="input"
+                        type="text"
+                        placeholder="Nombre"
+                    />
+                    <input
+                        name="email"
+                        className="input"
+                        type="text"
+                        placeholder="Correo"
+                    />
+                    <input
+                        name="password"
+                        className="input"
+                        type="password"
+                        placeholder="Contraseña"
+                    />
+                    <button className="button">Registrarme</button>
+                </form>
+                {/*<Link to="/login">*/}
+                {/*    Iniciar sesión*/}
+                {/*</Link>*/}
+            </section>
+        </section>
+    )
+
+}
+
+export default Register;
