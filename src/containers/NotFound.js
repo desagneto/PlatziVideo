1bc591e42418e30ba9b39d8a2f94400e16ca01c7
Andrés Aguilar Nieto <desagneto@gmail.com>
2020-01-14 14:36:04 -0500

    clase container not found}

---
+++ src/containers/NotFound.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function NotFound() {
+
+    return (
+        <React.Fragment>
+            <h1>404: Not Found</h1>
+            <h2>Regresa al home</h2>
+        </React.Fragment>
+    )
+}
+
+export default NotFound;
--- src/routes/App.js
+++ src/routes/App.js
@@ -3,6 +3,7 @@ import {BrowserRouter, Route, Switch} from "react-router-dom";
 import Home from "../containers/Home";
 import Login from "../containers/Login";
 import Register from "../containers/Register";
+import NotFound from "../containers/NotFound";
 
 function App() {
     return (
@@ -11,6 +12,7 @@ function App() {
                 <Route exact path="/" component={Home}/>
                 <Route exact path="/login" component={Login}/>
                 <Route exact path="/register" component={Register}/>
+                <Route component={NotFound}/>
             </Switch>
         </BrowserRouter>
     );
