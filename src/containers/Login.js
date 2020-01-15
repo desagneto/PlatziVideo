d4d018b0b5a5529c5b248541cd98a04365b5f68d
Andrés Aguilar Nieto <desagneto@gmail.com>
2020-01-15 11:11:45 -0500

    clase crear formulario de login

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -6,34 +6,52 @@ import '../assets/styles/components/Login.css'
 
 function Login() {
 
-    return(
+    const [form, setValues] = React.useState({
+        email: ''
+    });
+
+    const handleInput = e => {
+        setValues({
+            ...form,
+            [e.target.name]: e.target.value
+        })
+    };
+
+    const handleSubmit = e => {
+        e.preventDefault()
+        console.log(form)
+    };
+
+    return (
         <section className="login">
             <section className="login__container">
                 <h2>Inicia sesión</h2>
-                <form className="login__container--form">
+                <form className="login__container--form" onSubmit={handleSubmit}>
                     <input
                         name="email"
                         className="input"
                         type="text"
                         placeholder="Correo"
+                        onChange={handleInput}
                     />
                     <input
                         name="password"
                         className="input"
                         type="password"
                         placeholder="Contraseña"
+                        onChange={handleInput}
                     />
                     <button className="button">Iniciar sesión</button>
                     <div className="login__container--remember-me">
                         <label>
-                            <input type="checkbox" id="cbox1" value="first_checkbox" /> Recuérdame
+                            <input type="checkbox" id="cbox1" value="first_checkbox"/> Recuérdame
                         </label>
                         <a href="/">Olvidé mi contraseña</a>
                     </div>
                 </form>
                 <section className="login__container--social-media">
-                    <div><img src={googleIcon} alt="google icon" /> Inicia sesión con Google</div>
-                    <div><img src={twitterIcon} alt="twitter icon" /> Inicia sesión con Twitter</div>
+                    <div><img src={googleIcon} alt="google icon"/> Inicia sesión con Google</div>
+                    <div><img src={twitterIcon} alt="twitter icon"/> Inicia sesión con Twitter</div>
                 </section>
                 <p className="login__container--register">
                     No tienes ninguna cuenta {' '}
