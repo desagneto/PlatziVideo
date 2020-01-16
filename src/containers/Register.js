9a7ef4ba851296fded1141de660716a099960c8b
Andrés Aguilar Nieto <desagneto@gmail.com>
2020-01-16 12:18:08 -0500

    clase register

--- src/containers/Register.js
+++ src/containers/Register.js
@@ -4,28 +4,49 @@ import '../assets/styles/components/Register.css'
 
 function Register() {
 
-    return(
+    const [form, setValues] = React.useState({
+        email: '',
+        name: '',
+        password: ''
+    });
+
+    const handleInput = e =>{
+      setValues({
+          ...form,
+          [e.target.name]:e.target.value,
+      })
+    };
+
+    const handleSubmit = e =>{
+        e.preventDefault();
+        console.log(form);
+    };
+
+    return (
         <section className="register">
             <section className="register__container">
                 <h2>Regístrate</h2>
-                <form className="register__container--form">
+                <form className="register__container--form" onSubmit={handleSubmit}>
                     <input
                         name="name"
                         className="input"
                         type="text"
                         placeholder="Nombre"
+                        onChange={handleInput}
                     />
                     <input
                         name="email"
                         className="input"
                         type="text"
                         placeholder="Correo"
+                        onChange={handleInput}
                     />
                     <input
                         name="password"
                         className="input"
                         type="password"
                         placeholder="Contraseña"
+                        onChange={handleInput}
                     />
                     <button className="button">Registrarme</button>
                 </form>
