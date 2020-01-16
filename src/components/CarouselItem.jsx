cd1bf5b1ac49d9ac6af16421b2c24d37ddcefc1d
Andrés Aguilar Nieto <desagneto@gmail.com>
2020-01-16 14:29:49 -0500

    clase vista general del player

--- src/components/CarouselItem.jsx
+++ src/components/CarouselItem.jsx
@@ -6,6 +6,7 @@ import plusIcon from '../assets/static/plus-icon.png';
 import removeIcon from '../assets/static/remove-icon.png'
 import {connect} from "react-redux";
 import {deleteFavourite, setFavourite} from "../actions";
+import {Link} from 'react-router-dom';
 
 const CarouselItem = (props) => {
 
@@ -26,13 +27,20 @@ const CarouselItem = (props) => {
             <img className="carousel-item__img" src={cover} alt={title}/>
             <div className="carousel-item__details">
                 <div>
-                    <img className="carousel-item__details--img" src={playIcon} alt="Play Icon"/>
+
+                    <Link to={`/player/${id}`}>
+                        <img
+                            className="carousel-item__details--img"
+                            src={playIcon}
+                            alt="Play Icon"
+                        />
+                    </Link>
 
                     {isList ?
                         <img
                             className="carousel-item__details--img"
                             src={removeIcon}
-                            alt="Plus Icon"
+                            alt="Remove Icon"
                             onClick={() => handleDeleteFavourite(id)}
                         /> :
                         <img
